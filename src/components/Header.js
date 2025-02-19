5b97ffd6615d1dc788a020ec8adee3ee94a23c3c
Sridurga Kakinada <sridurgakakinada@gmail.com>
2025-02-19 00:41:41 -0600

    Fixed missing Link import in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";  // ✅ Import Link here
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
