ae18df24e04980042d11162d33a766de20f27ecb
Sridurga Kakinada <sridurgakakinada@gmail.com>
2025-02-19 00:39:33 -0600

    Fixed navigation links with React Router <Link>

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,11 +20,11 @@ const Header = () => {
 
         {/* Navigation Links */}
         <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
-          <li><a href="/" onClick={toggleMenu}>Home</a></li>
-          <li><a href="/about" onClick={toggleMenu}>About</a></li>
-          <li><a href="/experience" onClick={toggleMenu}>Experience</a></li>
-          <li><a href="/projects" onClick={toggleMenu}>Projects</a></li>
-          <li><a href="/contact" onClick={toggleMenu}>Contact</a></li>
+          <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+          <li><Link to="/experience" onClick={toggleMenu}>Experience</Link></li>
+          <li><Link to="/projects" onClick={toggleMenu}>Projects</Link></li>
+          <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
         </ul>
 
         {/* Mobile Menu Icon */}
